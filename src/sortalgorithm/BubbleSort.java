79a26fffd44737dc3809890eebf410cbb4a51bec
chi <1396739200@qq.com>
2019-11-05 13:56:19 +0800

    冒泡排序-单向和双向

--- src/sortalgorithm/BubbleSort.java
+++ src/sortalgorithm/BubbleSort.java
@@ -1,115 +1,69 @@
 package sortalgorithm;
 
 
+import java.util.Arrays;
+
 /**
  * 冒泡排序（正序）（稳定算法）
  * 引入一个bool变量来判断数组是否提前有序，从而减少不必要的排序；
  * 双向冒泡排序
  * 从前后两端同时遍历，增加排序的速度
- * 快速排序（不稳定算法）
- * 选取一个基数对序列进行排序，使得序列中比基数小的数在基数左侧，比基数大的在基数右侧，不断分割直至子序列长度为一，此时排序完成
  */
 public class BubbleSort {
-    int i, j, temp;
-    boolean flag = false;
+    public static void main(String[] args) {
+        int[] num = new int[300];
+        for (int i = 0; i < 300; i++) {
+            num[i] = (int) (Math.random() * 300);
+        }
+        int[] a = {1, 4, 5, 7, 2, 3, 4, 8, 7};
+        sort(num);
+        Arrays.stream(num).forEach(System.out::println);
+    }
 
-    //原始算法从前向后遍历
-    public void bubble(int[] a) {
-        if (a == null || a.length <= 1) return;
-        flag = false;//标识符用于改进冒泡排序
-        for (i = 0; i < a.length - 1; i++) {
-            for (j = 0; j < a.length - 1 - i; j++) {
+    public static void sort(int[] a) {//单向排序
+        boolean isOk;
+        for (int i = 0, len = a.length; i < len - 1; i++) {
+            isOk = true;
+            for (int j = 0; j < len - i - 1; j++) {
                 if (a[j] > a[j + 1]) {
-                    flag = true;
-                    a[j] = a[j] ^ a[j + 1];
-                    a[j + 1] = a[j] ^ a[j + 1];
-                    a[j] = a[j] ^ a[j + 1];
+                    swap(a, j, j + 1);
+                    isOk = false;
                 }
             }
-            if (!flag)
+            if (isOk)
                 break;
-            else flag = false;
         }
     }
 
-    //原始算法从后向前遍历
-    public void bubblepre(int[] a) {
-        if (a == null || a.length <= 1) return;
-        flag = false;//标识符用于改进冒泡排序
-        for (i = 0; i < a.length - 1; i++) {
-            for (j = a.length - 2; j >= i; j--) {
+    public static void doubleSort(int[] a) {//双向冒泡
+        boolean isOk;
+        for (int i = 0, len = a.length; i < len - 1; i++) {
+            isOk = true;
+            for (int j = 0; j < len - i - 1; j++) {
                 if (a[j] > a[j + 1]) {
-                    flag = true;
-                    a[j] = a[j] ^ a[j + 1];
-                    a[j + 1] = a[j] ^ a[j + 1];
-                    a[j] = a[j] ^ a[j + 1];
+                    swap(a, j, j + 1);
+                    isOk = false;
                 }
             }
-            if (!flag)
+            if (isOk)
                 break;
-            else flag = false;
+        }
+        for (int i = a.length - 1; i > 0; i--) {
+            isOk = true;
+            for (int j = a.length - 1, len = a.length; j >= len - i; j--) {
+                if (a[j] < a[j - 1]) {
+                    swap(a, j, j - 1);
+                    isOk = false;
+                }
+            }
+            if (isOk)
+                break;
         }
     }
 
-    //使用双向排序改进冒泡排序
-    public void bothWay(int[] a) {
-        if (a == null || a.length <= 1) return;
-        int preIndex = 0, backIndex = a.length - 1, i = 0;
-        flag = false;
-        while (preIndex < backIndex) {
-            for (j = i; j < a.length - 1 - i; j++) {
-                if (a[j] > a[j + 1]) {
-                    flag = true;
-                    a[j] = a[j] ^ a[j + 1];
-                    a[j + 1] = a[j] ^ a[j + 1];
-                    a[j] = a[j] ^ a[j + 1];
-                }
-
-            }
-            backIndex--;
-            if (!flag) {
-                break;
-            } else flag = false;
-            for (j = a.length - 2 - i; j >= i; j--) {//
-                if (a[j] > a[j + 1]) {
-                    flag = true;
-                    a[j] = a[j] ^ a[j + 1];
-                    a[j + 1] = a[j] ^ a[j + 1];
-                    a[j] = a[j] ^ a[j + 1];
-                }
-            }
-            preIndex++;
-            if (!flag) {
-                break;
-            } else flag = false;
-            i++;//记录已经移动的位数以减少遍历的元素
-        }
-    }
-
-    public void quicksort(int[] a, int preIndex, int backIndex) {//合并了两次交换的步骤
-        if (a == null || a.length <= 1) return;
-        int index = preIndex;
-        int back = backIndex;
-        if (preIndex >= backIndex) return;//递归结束条件
-        while (preIndex < backIndex) {
-            while (preIndex < backIndex && a[index] <= a[backIndex]) {//从右向左找到第一个小于索引的值的位置
-                backIndex--;
-            }
-            while (preIndex < backIndex && a[index] >= a[preIndex]) {//从左向右找到第一个大于索引的位置
-                preIndex++;
-            }
-            if (preIndex < backIndex) {//如果大于索引的值在小于索引的值的左边则交换
-                a[backIndex] = a[backIndex] ^ a[preIndex];
-                a[preIndex] = a[backIndex] ^ a[preIndex];
-                a[backIndex] = a[backIndex] ^ a[preIndex];
-            }
-        }
-        a[backIndex] = a[index] ^ a[backIndex];//最后交换索引和backIndex(此时backIndex指向的值为从右至左第一个小于索引的值）的值
-        a[index] = a[index] ^ a[backIndex];
-        a[backIndex] = a[index] ^ a[backIndex];
-        if (index != backIndex)//将序列不断分割直至序列长度为一
-            quicksort(a, index, backIndex);
-        if (preIndex + 1 != back)
-            quicksort(a, preIndex + 1, back);
+    public static void swap(int[] a, int i, int j) {
+        a[i] = a[i] ^ a[j];
+        a[j] = a[i] ^ a[j];
+        a[i] = a[i] ^ a[j];
     }
 }
