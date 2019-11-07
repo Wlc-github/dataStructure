8a2d63097ba1c78376e3e9f9cb1982784b16f3c4
chi <1396739200@qq.com>
2019-11-07 09:25:59 +0800

    哈夫曼树

--- src/tree/HuffMan.java
+++ src/tree/HuffMan.java
@@ -34,34 +34,3 @@ public class HuffMan {
         createHuffMan(a).preOrder();
     }
 }
-
-class Node implements Comparable<Node> {
-    int val;
-    Node left;
-    Node right;
-
-    public Node(int val) {
-        this.val = val;
-    }
-
-    @Override
-    public String toString() {
-        return "Node{" +
-                "val=" + val +
-                '}';
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        return this.val - o.val;
-    }
-
-    public void preOrder() {//先序遍历二叉树
-        if (this.left == null && this.right == null)//只输出叶子结点
-            System.out.print(this.val + " ");
-        if (this.left != null)
-            this.left.preOrder();
-        if (this.right != null)
-            this.right.preOrder();
-    }
-}
